bb4790f630b28fda7ac16064f06019c70b12b611
meet3475 <meetdobariya480@gmail.com>
2024-08-20 11:04:06 +0530

    done

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,4 +1,4 @@
 export const baseURL = "http://localhost:8000/"
 export const variantURL = "http://localhost:8000/"
 
-export const authURL = "http://localhost:8000/api/v1/"
+export const authURL = "https://fruitable-server-sigma.vercel.app/api/v1/"
